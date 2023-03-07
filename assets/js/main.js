2681c081f27c433ddd9f52378d59d2d96a0e7c5d
RobertoNesta <robertonesta1993@gmail.com>
2023-03-07 16:01:58 +0100

    add the ; marks at the end of every line of code

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,11 +13,11 @@ const color = prompt("Qual è il tuo colore preferito?")
 console.log (color);
 
 //Infine scrivi sulla pagina il risultato usando questo formato: nomecognomecolorepreferito23
-const pwd = userName + surname + color + 23
+const pwd = userName + surname + color + 23;
 
-console.log(pwd)
+console.log(pwd);
 
-let element = document.getElementById("password")
-console.log (element)
-element.innerHTML = "password: " + pwd
+let element = document.getElementById("password");
+console.log (element);
+element.innerHTML = "password: " + pwd;
 
