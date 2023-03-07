82d697a2a809784802152a358d5417d7169e8300
RobertoNesta <robertonesta1993@gmail.com>
2023-03-07 15:58:57 +0100

    generating the password  variable and putting it as h1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,23 @@
 //password generator
 
 //Chiedi all'utente il suo nome
-let userName = prompt("Qual è il tuo nome?")
+const userName = prompt("Qual è il tuo nome?")
 console.log (userName);
 
 //poi chiedi il suo cognome
-let surname = prompt("Qual è il tuo cognome?")
+const surname = prompt("Qual è il tuo cognome?")
 console.log (surname);
 
 //poi chiedi il suo colore preferito
-let color = prompt("Qual è il tuo colore preferito?")
+const color = prompt("Qual è il tuo colore preferito?")
 console.log (color);
 
 //Infine scrivi sulla pagina il risultato usando questo formato: nomecognomecolorepreferito23
+const pwd = userName + surname + color + 23
+
+console.log(pwd)
+
+let element = document.getElementById("password")
+console.log (element)
+element.innerHTML = "password: " + pwd
+
