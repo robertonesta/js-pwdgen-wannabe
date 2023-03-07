e10f5555d110742387c63a399f2539d07d75869a
RobertoNesta <robertonesta1993@gmail.com>
2023-03-07 15:24:50 +0100

    add the user name prompt

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+//password generator
+
+//Chiedi all'utente il suo nome
+let userName = prompt("Qual è il tuo nome?")
+console.log (userName);
+
+//poi chiedi il suo cognome,
+//poi chiedi il suo colore preferito
+//Infine scrivi sulla pagina il risultato usando questo formato: nomecognomecolorepreferito23
