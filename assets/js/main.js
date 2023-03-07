724133f22ba2266a97fab57dceafe5759ec33459
RobertoNesta <robertonesta1993@gmail.com>
2023-03-07 15:29:20 +0100

    adding the surname and favorite color variable

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,12 @@
 let userName = prompt("Qual è il tuo nome?")
 console.log (userName);
 
-//poi chiedi il suo cognome,
+//poi chiedi il suo cognome
+let surname = prompt("Qual è il tuo cognome?")
+console.log (surname);
+
 //poi chiedi il suo colore preferito
+let color = prompt("Qual è il tuo colore preferito?")
+console.log (color);
+
 //Infine scrivi sulla pagina il risultato usando questo formato: nomecognomecolorepreferito23
